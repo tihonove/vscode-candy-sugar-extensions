ae48a83f21ac2275580162ef29d146a317797a23
Eugene Tikhonov <tihonov.ea@gmail.com>
2018-09-15 13:38:58 +0500

    Add initial code :boom:

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "presets": [
+        [
+            "@babel/preset-env",
+            {
+                "targets": {
+                    "node": "8.0.0"
+                }
+            }
+        ],
+        "@babel/preset-typescript"
+    ],
+    "plugins": [
+        "@babel/plugin-proposal-object-rest-spread",
+        ["@babel/plugin-proposal-decorators", { "legacy": true }],
+        ["@babel/plugin-proposal-class-properties", { "loose": true }]
+    ]
+};
