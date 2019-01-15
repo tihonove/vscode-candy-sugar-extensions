e02c13b36af57f40453b2be566178d3c41ecac5c
Eugene Tikhonov <tihonov.ea@gmail.com>
2019-01-16 02:25:36 +0500

    Поправил определение Checkbox

--- server/src/SugarElements/DefaultSugarElementInfos/DataElements/checkbox.ts
+++ server/src/SugarElements/DefaultSugarElementInfos/DataElements/checkbox.ts
@@ -6,7 +6,7 @@ export const checkbox: SugarElementInfo = {
     name: "checkbox",
     attributes: [
         visibilityPathAttribute,
-        { name: "type", valueTypes: [AttributeType.String] },
+        { name: "type", valueTypes: [AttributeType.Type] },
         { name: "defaultValue", valueTypes: [AttributeType.String] },
         { name: "checkedValue", valueTypes: [AttributeType.String] },
         { name: "uncheckedValue", valueTypes: [AttributeType.String] },
