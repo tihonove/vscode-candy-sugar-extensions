cf94bcc00394e269b523728bd8edc9b7911fd316
Eugene Tihonov <tihonov.ea@gmail.com>
2019-08-03 18:30:25 +0500

    Построение документации из генератора в CompileTime-е

---
+++ server/src/SugarElements/CompileTimeDocumentation/CompileTimeDocumentationFromGenerator.ts
@@ -0,0 +1,7 @@
+import { SugarElementInfo } from "../SugarElementInfo";
+
+// @ts-ignore
+import elementsFromGeneratorJson from "./CompileTimeDocumentationFromGeneratorSource";
+
+// tslint:disable-next-line no-unsafe-any
+export const elementsFromGenerator: SugarElementInfo[] = elementsFromGeneratorJson;
--- server/src/SugarElements/SugarElements.ts
+++ server/src/SugarElements/SugarElements.ts
@@ -1,9 +1,6 @@
-import { getDocumentation } from "@kontur.candy/generator";
-
+import { elementsFromGenerator } from "./CompileTimeDocumentation/CompileTimeDocumentationFromGenerator";
 import { legacyElements } from "./LegacyElements/LegacyElements";
 import { SugarElementInfo } from "./SugarElementInfo";
 import { tourElements } from "./TourElements/TourElements";
-
-const elementsFromGenerator: SugarElementInfo[] = getDocumentation();
 
 export const standardElements: SugarElementInfo[] = [...elementsFromGenerator, ...legacyElements, ...tourElements];
